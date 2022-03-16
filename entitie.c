ba953f1b27361d4f009ae72654a3bd8daf1ea791
David Meresan <meresan02david1006@gmail.com>
2022-03-16 13:43:15 +0200

    alocare dinamică

--- entitie.c
+++ entitie.c
@@ -37,5 +37,5 @@ int equal(Item* a, Item* b){
 
 
 void destroy_item(Item* it){
-    free(it);
+    ///free(it);
 }
--- main.c
+++ main.c
@@ -1,10 +1,17 @@
-#include <stdio.h>
+#define _CRTDBG_MAP_ALLOC
 #include "tests.h"
 #include "cons.h"
-
+#include <crtdbg.h>
+#include <stdio.h>
+#include <malloc.h>
 int main() {
     run_all_tests();
     Console* console = run();
     end_cons(console);
+    int *b = malloc(sizeof (int));
+
+    _CrtSetReportMode( _CRT_ERROR, _CRTDBG_MODE_DEBUG );
+    int a =  _CrtDumpMemoryLeaks();
+    printf("%d", a);
     return 0;
 }
